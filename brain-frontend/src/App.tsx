81cf1197f6b50b9675f93ae3f08f231e0560a31b
Aniket Vishwakarma <aniketvishwakarma2004@gmail.com>
2025-02-25 11:46:07 +0530

    theme toggle setup

--- brain-frontend/src/App.tsx
+++ brain-frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { ThemeProvider } from "./Components/Theme/ThemeProvider"
 import Home from "./Pages/Home"
-
+import { useState,useEffect } from "react";
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.documentElement.classList.add(savedTheme);
+    setLoading(false);
+  }, []);
   return (
     <>
-    <Home/>
+     <ThemeProvider>
+       <Home/>
+     </ThemeProvider>
     </>
   )
 }
--- brain-frontend/src/Components/Navbar.tsx
+++ brain-frontend/src/Components/Navbar.tsx
@@ -1,18 +1,20 @@
 import {Brain} from 'lucide-react'
 import { Button } from './ui/Button'
+import ThemeToggle from './Theme/ThemeToggle'
 
 const Navbar = () => {
   return (
-    <header className="bg-gray-50 dark:bg-slate-950 flex justify-around items-center text-white min-h-14">
+    <header className="bg-gray-50 dark:bg-slate-950 flex justify-around items-center dark:text-white text-neutral-950 min-h-14">
         <div className='flex gap-2'>
             <Brain/>
             <span>Brainn</span>
         </div>
         <div className='flex'>
             <input type="text" />
+            <ThemeToggle/>
             <ul className='flex gap-2 justify-center items-center'>
                 <li>Features</li>
-                <Button>Sign up</Button>
+                <Button variant="outline">Sign up</Button>
             </ul>
         </div>
     </header>
